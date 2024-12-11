6124c3f3600857617da9b0368fb78a43fb71431c
Ali Suliman <alisuliman@Alis-MacBook-Pro.local>
2024-12-11 20:46:01 +0100

    feat: initialize DB on server start

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,20 @@
 import app from "./app";
+import { initDB } from "./db/utils/initDB";
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () =>
-  console.log(`Server running at PORT: ${PORT} \nurl: http://localhost:${PORT}`)
-);
+const startServer = async () => {
+  try {
+    await initDB();
+    console.log("+ Database setup complete.");
+
+    app.listen(PORT, () =>
+      console.log(`Server running at PORT: ${PORT} \nurl: http://localhost:${PORT}`)
+    );
+  } catch (e) {
+    console.error("- Failed to start the server: ", e);
+    process.exit(1)
+  }
+}
+
+startServer();
