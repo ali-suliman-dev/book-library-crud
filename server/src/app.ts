c8aa34e9bf3890216bc8adadd2183d3f6d1033c1
Ali Suliman <alisuliman@Alis-MacBook-Pro.local>
2024-12-12 00:17:34 +0100

    feat: add basic cors handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,13 @@ import express from "express";
 import bookRouter from "./application/routes/books.routes"
 import swaggerUi from "swagger-ui-express";
 import swaggerConfig from "./docs/swagger/config";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+  origin: "*",
+}));
 
 app.use(express.json());
 app.use("/books", bookRouter);
